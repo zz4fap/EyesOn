557fcefd33b3328201a68e4ef8cbf846de8c99f4
VPN - Projeto Fapesp Inatel <fapesp-inatel@local.inatel.br>
2024-08-27 19:21:28 -0300

    Mudanças no click

--- v6.5/MainCode/amb_teste/teste_dlib.py
+++ v6.5/MainCode/amb_teste/teste_dlib.py
@@ -6,78 +6,101 @@ import dlib
 import cv2
 import pyautogui as pag
 import time
+from math import hypot
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+def midpoint(p1 ,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
-# start the video stream and allow the camera to warm up
-video_stream = cv2.VideoCapture(0)
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
 
-start_time = None
-trigger_time = 2  # seconds
+    hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_length = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
 
-while True:
-    # capture the frame from the video stream
-    ret, frame = video_stream.read()
+    ratio = hor_line_length / ver_line_length
+    return ratio
 
-    # check if frame is captured properly
-    if not ret:
-        break
+def click_mouse():
+    # initialize dlib's face detector (HOG-based) and then create
+    # the facial landmark predictor
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-    # resize the frame and convert it to grayscale
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # start the video stream and allow the camera to warm up
+    video_stream = cv2.VideoCapture(0)
 
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-    #print(rects)
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+    start_time = None
+    trigger_time = 1  # seconds
 
-        # draw the face landmarks on the frame
-        #print(shape)
-        #fds = shape[38:41]
-        l1, l2 = shape[37], shape[38]
-        l3, l4 = shape[41], shape[40]
+    while True:
+        # capture the frame from the video stream
+        ret, frame = video_stream.read()
 
-        r1, r2 = shape[43], shape[44]
-        r3, r4 = shape[47], shape[46]
-        fds = [l1, l2, l3, l4, r1, r2, r3, r4]
-        #print(fds)
-        #print(l1[1] - l3[1])
-        #print(l2[1] - l4[1])
+        # check if frame is captured properly
+        if not ret:
+            break
 
-        left_eye = (l1[1] - l3[1] + l2[1] - l4[1])/2
-        right_eye = (r1[1] - r3[1] + r2[1] - r4[1])/2
+        # resize the frame and convert it to grayscale
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if left_eye >= -4:
-            if start_time is None:
-                start_time = time.time()  # start the timer
-            elif time.time() - start_time >= trigger_time:
-                pag.click()  # trigger the click if the condition is met for the required time
-                start_time = None  # reset the timer after the action is triggered
-        else:
-            start_time = None  # reset the timer if the condition is not met
+        # detect faces in the grayscale image
+        rects = detector(gray, 1)
+        #print(rects)
+        # loop over the face detections
+        for (i, rect) in enumerate(rects):
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy array
+            shape = predictor(gray, rect)
+            t = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            # draw the face landmarks on the frame
+            #print(shape)
+            #det = shape[38:41]
+            l1, l2 = shape[37], shape[38]
+            l3, l4 = shape[41], shape[40]
+
+            r1, r2 = shape[43], shape[44]
+            r3, r4 = shape[47], shape[46]
+            det = [l1, l2, l3, l4, r1, r2, r3, r4]
+            #print(fds)
+            #print(l1[1] - l3[1])
+            #print(l2[1] - l4[1])
+
+            left_eye = get_blinking_ratio([36, 37, 38, 39, 40, 41], t)
+            right_eye = get_blinking_ratio([42, 43, 44, 45, 46, 47], t)
+            blinking_ratio = (left_eye + right_eye) / 2
+            print(blinking_ratio)
+
+            #left_eye = (l1[1] - l3[1] + l2[1] - l4[1])/2
+            #right_eye = (r1[1] - r3[1] + r2[1] - r4[1])/2
+
+            if blinking_ratio > 5:
+                if start_time is None:
+                    start_time = time.time()  # start the timer
+                elif time.time() - start_time >= trigger_time:
+                    pag.click()  # trigger the click if the condition is met for the required time
+                    start_time = None  # reset the timer after the action is triggered
+            else:
+                start_time = None  # reset the timer if the condition is not met
 
 
-        #print(left_eye)
-        print(right_eye)
-        for (x, y) in fds:
-            cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+            #print(left_eye)
+            #print(right_eye)
+            for (x, y) in det:
+                cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
 
-    # show the frame
-    cv2.imshow("Frame", frame)
+        # show the frame
+        cv2.imshow("Frame", frame)
 
-    # if the 'q' key is pressed, break from the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # if the 'q' key is pressed, break from the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# cleanup
-video_stream.release()
-cv2.destroyAllWindows()
+    # cleanup
+    video_stream.release()
+    cv2.destroyAllWindows()
